5238ab824808a35358ea9ec0ded056b71881c11c
ManthanRanpariya <ranpariyamanthan9930@gmail.com>
2023-03-10 12:17:03 +0530

    Day-3

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import Home from './page/Home';
+import Title from './components/Title';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Title name="Title1"/>
+        <Title name="Title2"/>
+        <Title  name="Title3"/> */}
+        {/* <Home />
+         */}
+         <Title title="mathan"desc="is a Developer"/>
+      </div>
+    )
+  }
+}
